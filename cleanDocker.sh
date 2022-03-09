b0573f7f8e6d5b075f5bf1b4a3339857d69d335d
testA113 <42307911+aliharriss@users.noreply.github.com>
2022-03-10 11:10:45 +1300

    clean update

--- cleanDocker.sh
+++ cleanDocker.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
-docker rm -f $(docker ps -a -q)
-docker service rm $(docker service ls -q)
-docker image rm -f $(docker image ls -q)
+
+# Delete all docker resources on your machine for a fresh, empty clean docker
+
+# swarm only commands
+if [ "$(docker info --format '{{.Swarm.LocalNodeState}}')" == "active" ]; then
+  echo "in swarm mode"
+  echo "🐳 deleting services"
+  services=$(docker service ls -q)
+  if [ "${#services}" != "0" ]; then 
+    docker service rm $services
+  fi
+  echo "🐳 deleting secrets"
+  secrets=$(docker secret ls -q)
+  if [ "${#secrets}" != "0" ]; then 
+    docker secret rm $secrets
+  fi
+  echo "🐳 deleting configs"
+  configs=$(docker config ls -q)
+  if [ "${#configs}" != "0" ]; then 
+    docker config rm $configs
+  fi
+fi
+# standalone and swarm commands
+echo "🐳 deleting containers"
+containers=$(docker ps -a -q)
+if [ "${#containers}" != "0" ]; then 
+  docker rm -f $containers
+fi
+echo "🐳 deleting images"
+images=$(docker image ls -q)
+if [ "${#images}" != "0" ]; then 
+  docker image rm -f $images
+fi
+echo "🐳 deleting volumes"
 docker volume prune -f
+echo "🐳 deleting networks"
 docker network prune -f 
-docker secret rm $(docker secret ls -q)
-docker config rm $(docker config ls -q)
+echo "🐳 deleting cache"
+docker builder prune -f
